0c20d73bb0a224b250b781dfcfd06ecebbec2870
Biser Atanasov <biser.p.atanasov@gmail.com>
2016-08-02 15:23:47 +0300

    Minor changes

--- scripts/app.js
+++ scripts/app.js
@@ -1,36 +1,19 @@
-var map;
-function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 42.0209, lng: 23.0943},
-        zoom: 2
-    });
-    var infoWindow = new google.maps.InfoWindow({map: map});
+var map = new ol.Map({
+    controls: ol.control.defaults().extend([
+        new ol.control.FullScreen()
+    ]),
+    target: 'map',
+    layers: [
+        new ol.layer.Tile({
+            source: new ol.source.OSM()
+        })
+    ],
+    view: new ol.View({
+        center: ol.proj.fromLonLat([37.41, 8.82]),
+        zoom: 4
+    })
+});
 
-    // Try HTML5 geolocation.
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            };
-
-            infoWindow.setPosition(pos);
-            infoWindow.setContent("You are here!");
-            map.setCenter(pos);
-            map.setZoom(14);
-        }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-        });
-    } else {
-        // Browser doesn't support Geolocation
-        handleLocationError(false, infoWindow, map.getCenter());
-    }
-}
-
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-    infoWindow.setPosition(pos);
-    infoWindow.setContent(browserHasGeolocation ?
-        'Error: The Geolocation service failed.' :
-        'Error: Your browser doesn\'t support geolocation.');
-}
-
+$('.ol-zoom-in, .ol-zoom-out').tooltip({
+    placement: 'right'
+});
