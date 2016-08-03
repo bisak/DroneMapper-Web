756a5b2633a3705b6c195e179fcfe0ec88f31205
Biser Atanasov <biser.p.atanasov@gmail.com>
2016-08-03 14:37:43 +0300

    Minor tweaks, changes and bug fixes

--- scripts/app.js
+++ scripts/app.js
@@ -1,19 +1,39 @@
-var map = new ol.Map({
-    controls: ol.control.defaults().extend([
-        new ol.control.FullScreen()
-    ]),
-    target: 'map',
-    layers: [
-        new ol.layer.Tile({
-            source: new ol.source.OSM()
-        })
-    ],
-    view: new ol.View({
-        center: ol.proj.fromLonLat([37.41, 8.82]),
-        zoom: 4
-    })
+/*JavaScript*/
+
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYmlza2F6eiIsImEiOiJjaXJkOTFkb3owMDdxaTltZ21vemsxcGViIn0.70mwo4YYnbxY_BJoEsGYxw', {attribution: '&copy; <a href="https://www.mapbox.com">Mapbox</a>'}),
+    outdoors = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYmlza2F6eiIsImEiOiJjaXJkOTFkb3owMDdxaTltZ21vemsxcGViIn0.70mwo4YYnbxY_BJoEsGYxw', {attribution: '&copy; <a href="https://www.mapbox.com">Mapbox</a>'}),
+    dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYmlza2F6eiIsImEiOiJjaXJkOTFkb3owMDdxaTltZ21vemsxcGViIn0.70mwo4YYnbxY_BJoEsGYxw', {attribution: '&copy; <a href="https://www.mapbox.com">Mapbox</a>'}),
+    light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYmlza2F6eiIsImEiOiJjaXJkOTFkb3owMDdxaTltZ21vemsxcGViIn0.70mwo4YYnbxY_BJoEsGYxw', {attribution: '&copy; <a href="https://www.mapbox.com">Mapbox</a>'}),
+    satelite = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYmlza2F6eiIsImEiOiJjaXJkOTFkb3owMDdxaTltZ21vemsxcGViIn0.70mwo4YYnbxY_BJoEsGYxw', {attribution: '&copy; <a href="https://www.mapbox.com">Mapbox</a>'}),
+    sateliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYmlza2F6eiIsImEiOiJjaXJkOTFkb3owMDdxaTltZ21vemsxcGViIn0.70mwo4YYnbxY_BJoEsGYxw', {attribution: '&copy; <a href="https://www.mapbox.com">Mapbox</a>'})
+    ;
+
+let map = L.map('map', {
+    center: [0, 0],
+    zoom: 14,
+    layers: streets,
+    fullscreenControl: true,
 });
 
-$('.ol-zoom-in, .ol-zoom-out').tooltip({
-    placement: 'right'
-});
+
+let lc = L.control.locate({
+    strings: {
+        title: "Locate",
+    },
+    keepCurrentZoomLevel: true
+}).addTo(map);
+
+map.zoomControl.setPosition('bottomright');
+
+lc.start();
+
+let baseMaps = {
+    "Streets": streets,
+    "Outdoors": outdoors,
+    "Dark": dark,
+    "Light": light,
+    "Satelite": satelite,
+    "Satelite Streets": sateliteStreets
+};
+
+L.control.layers(baseMaps).addTo(map);
